48de4a2c8c61cf3b1ee4ecc2255755816a57d083
HA0-TUK <hayoungtuk@gmail.com>
2025-09-18 15:23:46 +0900

    사각운동

--- character_moves.py
+++ character_moves.py
@@ -7,7 +7,54 @@ grass = load_image('grass.png')
 character = load_image('character.png')
 
 #캐릭터 사각 운동, 삼각 운동, 원운동을 무한 반복
+x, y = 400, 90 #초기위치
+
+#원운동 변수
+radius = 255
+center_x, center_y = 400, 345
+
+#사각운동 함수
+def move_square():
+        # 우하단 이동 
+    while x < 800:
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        x += 2
+        delay(0.01)
+
+    # 우상단 이동
+    while y < 600:
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        y += 2
+        delay(0.01)
+
+    # 좌상단 이동
+    while x > 0:
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        x -= 2
+        delay(0.01)
+
+    # 좌하단 이동
+    while y > 90:
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        y -= 2
+        delay(0.01)
+
+    # 중앙복귀
+    while x < 400:
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        x += 2
+        delay(0.01)
 
 #무한반복
 while True:
-    pass
+    move_square()
