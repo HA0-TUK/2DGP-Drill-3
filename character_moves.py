77f3eca4c6209cb94eb6b81c94c5a1933ddc6f88
HA0-TUK <hayoungtuk@gmail.com>
2025-09-18 15:17:47 +0900

    전체 무한루프 + sprite load

---
+++ character_moves.py
@@ -0,0 +1,13 @@
+
+from pico2d import *
+import math
+
+open_canvas()
+grass = load_image('grass.png')
+character = load_image('character.png')
+
+#캐릭터 사각 운동, 삼각 운동, 원운동을 무한 반복
+
+#무한반복
+while True:
+    pass
